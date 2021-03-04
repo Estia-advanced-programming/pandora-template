77a6a414a7d8c5a965abd436207d7d3351130c5a
dhmmasson <dhm.masson+github@gmail.com>
2021-03-04 15:11:44 +0100

    [Doc] Comment Analysis.java to better explain the featureValue map
    Explain a bit better how the analysis.java works.
    
    The Map ```featureValues```associate the name of the feature with the textual representation to be printed.
    Highlight that currently the flightDuration is wrongly stored for printing

--- src/main/java/fr/estia/pandora/model/Analysis.java
+++ src/main/java/fr/estia/pandora/model/Analysis.java
@@ -4,17 +4,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @author <@dhmmasson> 
- * Generic Analysis, you should create specialized analysis that extends this 
+ * @author <@dhmmasson>
+ * Generic Analysis, you should create specialized analysis
+ * that extends this class for the different milestones
  */
 public class Analysis {
 	private Flight flight ;
+	//List of feature computed by this analysis
+	//The Map associate the name of the feature with the textual representation to be printed
 	private Map<String, String> featureValues ;
-	//List of feature computed by this analysis 
-	private int flightDuration ;	
-	
-	
-	
+
+	private int flightDuration ;
+
+
+
 	/**
 	 * Constructor, create a basic analysis 
 	 * @param flight
@@ -26,17 +29,22 @@ public class Analysis {
 	} 
 	
 	public String getFeatureValue( String feature ) {
-		String value = "" ; 
+		String value = "" ;
 		if( featureValues.containsKey( feature ) ) {
-			value = featureValues.get( feature ) ; 
+			value = featureValues.get( feature ) ;
 		}
-		return value ; 
+		return value ;
 	}
-	
-	
+
+	/*
+		This section compute the flight duration by taking the difference of timestamp
+		between the first and last records. The difference is store in s in a variable for future computation.
+		in the map the duration in second is stored
+		TODO: for the feature -o flightDuration, the format is wrong it should be HH:mm:ss
+	*/
 	private void computeFlightDuration() {
-		int startTime, endTime ; 
-		startTime = flight.getRecords().get( 0 ).getTimestamp() ; 
+		int startTime, endTime ;
+		startTime = flight.getRecords().get( 0 ).getTimestamp() ;
 		endTime = flight.getRecords().get( flight.getRecords().size() -1 ) .getTimestamp() ;
 		setFlightDuration(endTime - startTime) ;
 	}
@@ -44,15 +52,20 @@ public class Analysis {
 		return flightDuration;
 	}
 	public void setFlightDuration(int flightDuration) {
+		//Store for further computation where flight duration in second might be useful
 		this.flightDuration = flightDuration;
+		//TODO: change to store with the correct format HH:mm:ss
 		this.featureValues.put( "flightDuration", String.valueOf( flightDuration ) ) ;
 	}
-	
+
+	/**
+	 * @return a string that represents all the feature of the analysis and their value
+	 */
 	@Override
 	public String toString() {
 		String description = "" ;
 		for (String feature : featureValues.keySet() ) {
-			description += feature + ": " + featureValues.get( feature ) + "\n"; 			
+			description += feature + ": " + featureValues.get( feature ) + "\n";
 		}
 		return description;
 	}
